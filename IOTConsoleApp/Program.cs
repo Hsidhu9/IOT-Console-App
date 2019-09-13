14ed49f64e3c08d433670a0e47f9051a745505fe
DESKTOP-EFIJVV4\hsidh <hsidhu@my.gcu.edu>
2019-09-12 22:11:25 -0700

    Add project files.

---
+++ IOTConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IOTConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
